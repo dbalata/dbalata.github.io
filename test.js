cf15d4f52de26153f9f933aaed2a8990b8323908
Dylan <dbalata@unm.edu>
2017-12-14 21:51:56 -0700

    squashed

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 var a = 10;
 
-document.write(a + " factorial is: ");
-document.write(fact(a) + "<br>");
+function run()
+{
+	document.write(a + " factorial is: ");
+	document.write(fact(a) + "<br>");
+}
 
 function fact(n)
 {
-  if(n == 1) return 1;
-  else return n * fact(n - 1);
+	if(n == 1) return 1;
+	else return n * fact(n - 1);
 }
+
