21a54037b3b3992983db876623f1d1170e1ce7f3
Dylan <dbalata@unm.edu>
2017-12-14 22:22:49 -0700

    squashed

--- test.js
+++ test.js
@@ -2,8 +2,9 @@ var a = 10;
 
 function run()
 {
-	document.write(a + " factorial is: ");
-	document.write(fact(a) + "<br>");
+	//document.write(a + " factorial is: ");
+	//document.write(fact(a) + "<br>");
+	window.location.href = "http://stackoverflow.com";
 }
 
 function fact(n)
